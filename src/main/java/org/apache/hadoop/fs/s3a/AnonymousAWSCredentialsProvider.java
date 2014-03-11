4459f0f58a61d7c50834f80059e35ea4605529cb
Jordan Mendelson <jordan@zenzen.org>
2014-03-10 17:37:30 -0700

    Bump to 0.0.2

--- src/main/java/org/apache/hadoop/fs/s3a/AnonymousAWSCredentialsProvider.java
+++ src/main/java/org/apache/hadoop/fs/s3a/AnonymousAWSCredentialsProvider.java
@@ -22,7 +22,7 @@ import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AnonymousAWSCredentials;
 import com.amazonaws.auth.AWSCredentials;
 
-public class AnonymousAWSCredentialsProvider  implements AWSCredentialsProvider {
+public class AnonymousAWSCredentialsProvider implements AWSCredentialsProvider {
   public AWSCredentials getCredentials() {
     return new AnonymousAWSCredentials();
   }
